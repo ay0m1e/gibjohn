9b8f177e46a23d01d93de369478db228ce3fd9e7
Ayomide <ay0m1ealonge@gmail.com>
2026-02-12 12:17:42 +0000

    COntinue dev of the register and login api....Now to begin testing

--- api/auth/register.php
+++ api/auth/register.php
@@ -12,14 +12,20 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST'){
 }
 
 
-$email = t1rim($_POST['email'] ?? '');
+$email = trim($_POST['email'] ?? '');
 $password = $_POST ['password'] ?? '';
 $role = $_POST['role'] ?? 'learner';
 
 
-if (!$email || !$password) {
+if (!$email || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
     http_response_code(400);
-    echo json_encode(['error' => 'Email and password required']);
+    echo json_encode(['error' => 'VAlid email required']);
+    exit;
+}
+
+if (strlen($password) < 6) {
+    http_response_code(400);
+    echo json_encode(['error' => 'Password must be at least 6 characters']);
     exit;
 }
 
@@ -28,3 +34,46 @@ if (!in_array($role, ['learner', 'tutor'])) {
     echo json_encode(['error' => 'Invalid role']);
     exit;
 }
+
+
+$passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+try {
+    stmt = $pdo->prepare(
+        "INSERT INTO users (email, password_hash, role)
+        VALUES (:email, :password_hash, :role)"
+    );
+
+
+    $stmt -> execute([
+        'email' => $email,
+        'password_hash' => $passwordHash
+        'role' => $role
+    ]);
+
+    $userId = $pdo->lastInsertId();
+    if ($role === 'learner') {
+        $pdo->prepare(
+            "INSERT INTO learner_profiles (user_id)
+            VALUES (:user_id)"
+        )->execute(['user_id' => $userId]);
+    } else {
+        $pdo->prepare(
+            "INSERT INTO tutor_profiles (user_id)
+            VALUES (:user_id)"
+        )-> execute (['user_id' => $userId]);
+    }
+
+    echo json_encode(['success' => true]);
+} catch (PDOException $e){
+    if ($e->errorInfo[1] == 1062){
+        http_response_code(409);
+        echo json_encode(['error' => 'Email already exists']);
+    } else{
+        http_response_code(500);
+        echo json_encode(['error' =>'Registration failed']);
+    }
+}
+
+
+
